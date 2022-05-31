7a1cf7ca850c13fcc984ef8d3d411d21e21f2fac
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2022-05-31 22:21:28 +0300

    feat: add implementation for 'delete' function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,21 @@
-export const remove = async () => {
-    // Write your code here 
-};
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filesPath = path.join(__dirname, 'files');
+const fileToRemovePath = path.join(filesPath, 'fileToRemove.txt');
+
+export const remove = async (fileToRemove) => {
+  fs.access(fileToRemove, function (error) {
+    if (!error) {
+      fs.unlink(fileToRemove, (err) => {
+        if (err) throw new Error('FS operation failed');
+      });
+    } else throw new Error('FS operation failed');
+  });
+}
+
+remove(fileToRemovePath);
