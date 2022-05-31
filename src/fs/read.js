a1b67034d1c2b80f838d55de817ca75954a98597
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2022-05-31 22:38:28 +0300

    feat: add implementation for 'read' function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,19 @@
-export const read = async () => {
-    // Write your code here 
-};
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filesPath = path.join(__dirname, 'files');
+const fileToReadPath = path.join(filesPath, 'fileToRead.txt');
+
+export const read = async (filePath) => {
+  fs.access(filePath, async (error) => {
+    if (!error) {
+      const fileContent = await fs.promises.readFile(filePath, 'utf8');
+      console.log(fileContent);
+    } else throw new Error('FS operation failed');
+  });
+}
+
+read(fileToReadPath);
