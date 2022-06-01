3c2850978c968e92d6e04cf67116932af25914c4
SergeiOsmolovskii <osmolovskii_sergei@mail.ru>
2022-06-01 11:06:32 +0300

    feat: add implementation for 'env' function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,17 @@
 export const parseEnv = () => {
-    // Write your code here 
+  let args = process.argv;
+  let arg = 0;
+
+  args.forEach((val, index) => {
+    if (index > 1) {
+      if (index % 2 === 0 && val.includes('RSS_')) {
+        arg = val.slice(2);
+      } else if (arg !== 0) {
+        console.log(`${arg}=${val}`);
+        arg = 0;
+      }
+    }
+  });
 };
+
+parseEnv();
