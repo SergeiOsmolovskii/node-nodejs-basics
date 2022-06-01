cc48fc0df9cd67e47d5dd4dec3f1edb6127838d1
SergeiOsmolovskii <osmolovskii_sergei@mail.ru>
2022-06-01 12:54:39 +0300

    feat: add implementation for 'read stream' function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as crypto from 'crypto';
-import { fileURLToPath } from 'url'
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,11 +9,11 @@ const filesPath = path.join(__dirname, 'files');
 const fileToReadPath = path.join(filesPath, 'fileToCalculateHashFor.txt');
 
 export const calculateHash = async () => {
-    const fileToReadContent = await fs.promises.readFile(fileToReadPath, 'utf8');
-    const hash = crypto.createHash('sha256');
-    hash.update(fileToReadContent);
-    const fileHash = hash.digest('hex');
-    console.log(fileHash);
+  const fileToReadContent = await fs.promises.readFile(fileToReadPath, 'utf8');
+  const hash = crypto.createHash('sha256');
+  hash.update(fileToReadContent);
+  const fileHash = hash.digest('hex');
+  console.log(fileHash);
 };
 
 calculateHash();
--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,17 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filesPath = path.join(__dirname, 'files');
+const fileToReadPath = path.join(filesPath, 'fileToRead.txt');
+
 export const read = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(fileToReadPath, 'utf8');
+  readStream.on('data', (data) => {
+    process.stdout.write(data);
+  });
 };
+
+read();
