dc3131a26851f631b926d79562210dd68357752e
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2022-06-01 20:07:48 +0300

    fix: fix implementation for 'transform' function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,11 +1,17 @@
-import * as fs from 'fs';
-/* TO FIX */
-export const transform = async () => {
-  process.stdin.on('data', (data) => {
-    
-    process.stdout.write(data.toString().split('').reverse().join('').trim());
-    process.exit();
+import { Transform } from 'stream';
+
+const myTransform = () => {
+  return new Transform({
+    transform(chunk, encoding, callback) {
+      const transformResult = chunk.toString().split('').reverse().join('').trim();
+      callback(null, transformResult);
+      process.exit();
+    }
   });
+}
+
+export const transform = async () => {
+  process.stdin.pipe(myTransform()).pipe(process.stdout);
 };
 
 transform();
