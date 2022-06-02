e9338c798afc2821bf92dbc5ce7cc23cf18ae8ab
SergeiOsmolovskii <osmolovskii_sergei@mail.ru>
2022-06-02 16:27:21 +0300

    feat: add implementation for 'worker theads' functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,23 @@
+import * as worker from 'worker_threads';
+import * as os from 'os';
+
 export const performCalculations = async () => {
-    // Write your code here
+  const CPUCount = os.cpus().length;
+  const promisesArray = [];
+
+  for (let i = 1; i <= CPUCount; i++) {
+    promisesArray.push(new Promise((res, rej) => {
+      const newWorker = new worker.Worker('./wt/worker.js', {
+        workerData: i + 10
+      })
+      newWorker.on('message', res);
+      newWorker.on('error', rej);
+    }));
+  }
+
+  await Promise.allSettled(promisesArray).then((value) => {
+    console.log(value);
+  });
 };
+
+performCalculations();
