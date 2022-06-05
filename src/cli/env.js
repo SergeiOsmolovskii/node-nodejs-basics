ad847baaf978d232d05c2a6f776087fafc4ba8b8
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2022-06-05 21:47:46 +0300

    fix: rewrite 'env' function aсcording to the task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,15 +1,10 @@
 export const parseEnv = () => {
-  let args = process.argv;
-  let arg = 0;
-
-  args.forEach((val, index) => {
-    if (index > 1) {
-      if (index % 2 === 0 && val.includes('RSS_')) {
-        arg = val.slice(2);
-      } else if (arg !== 0) {
-        console.log(`${arg}=${val}`);
-        arg = 0;
-      }
+  let env = process.env;
+  const keys = Object.keys(env);
+  keys.forEach((val) => {
+    if (val.includes('RSS_')) {
+      let arg = val;
+      console.log(`${arg}=${env[val]}`);
     }
   });
 };
